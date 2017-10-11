a07339a6a733633bb2d363b8aa2b9ac883363505
Alex Lang <alekx@Alexs-MacBook-Pro.local>
2017-10-10 20:46:15 -0600

    init commit

---
+++ lib/teacher/movie_data.ex
@@ -0,0 +1,24 @@
+defmodule Teacher.MovieData do
+  import Plug.Conn
+  import Ecto.Query, only: [from: 2]
+
+  alias Teacher.Repo
+  alias TeacherWeb.Movie
+
+  def init(opts) do
+    Keyword.fetch(opts, :foo)
+  end
+
+  def call(conn, msg) do
+    movie_total = Repo.one(from m in Movie, select: count("*"))
+
+    case msg do
+      {:ok, msg} ->
+        custom_msg = "#{msg} #{movie_total}"
+        assign(conn, :movie_total_msg, custom_msg)
+      :error ->
+        default_msg = "We found #{movie_total} movies."
+        assign(conn, :movie_total_msg, default_msg)
+    end
+  end
+end
